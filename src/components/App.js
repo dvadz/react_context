40953a2f0dbbc5b578289ef9410557c8dfb1fe07
Daniel Vadil <dvadz@yahoo.com>
2022-04-19 16:14:57 -0400

    Added an onClick handler that changes the slected language when the flag icon is clicked

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,25 @@ import React from "react";
 class App extends React.Component {
   state = { language: "english" };
 
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
   render() {
     return (
       <div className='ui container'>
         <div>
           Select a language:
-          <i className='flag us'></i>
-          <i className='flag fr'></i>
+          <i
+            className='flag us'
+            onClick={() => {
+              this.onLanguageChange("english");
+            }}></i>
+          <i
+            className='flag fr'
+            onClick={() => {
+              this.onLanguageChange("french");
+            }}></i>
         </div>
       </div>
     );
