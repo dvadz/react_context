03ec250be3709b4e482416fe084c13efcf97f4cf
Daniel Vadil <dvadz@yahoo.com>
2022-04-21 15:37:09 -0400

    Imported LanguageContext into Field, added static contextType and used context to determine correct label for Name input

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,10 +1,14 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Field extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
+    const renderName = this.context === "english" ? "Name" : "Nom";
     return (
       <div className='ui field'>
-        <label>Name</label>
+        <label>{renderName}</label>
         <input />
       </div>
     );
