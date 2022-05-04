669dd42a20ccc6f9bf8dcd599a69d0a56c588366
Daniel Vadil <dvadz@yahoo.com>
2022-05-04 05:23:40 -0400

    Deleted state and onLanguage callback in App, they all now reside inside the LanguageStore

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,11 @@ import { LanguageStore } from "../contexts/LanguageContext";
 import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
-  state = { language: "english" };
-
-  onLanguageChange = (language) => {
-    this.setState({ language });
-  };
-
   render() {
     return (
       <div className='ui container'>
         <LanguageStore>
-          <LanguageSelector onLanguageChange={this.onLanguageChange} />
+          <LanguageSelector />
           <UserCreate />
         </LanguageStore>
       </div>
