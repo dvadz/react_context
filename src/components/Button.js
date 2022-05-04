cff0a2cdc17f1eafe6fcb4b042b0578429720b63
Daniel Vadil <dvadz@yahoo.com>
2022-05-04 05:48:05 -0400

    Also updated the usage of LanguageContext inside of Button to destructure "language"

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,14 +2,16 @@ import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
-  renderSubmit(value) {
-    return value === "english" ? "Submit" : "Envoyer";
+  renderSubmit(language) {
+    return language === "english" ? "Submit" : "Envoyer";
   }
 
   render() {
     return (
       <button className='ui button primary'>
-        <LanguageContext.Consumer>{(value) => this.renderSubmit(value)}</LanguageContext.Consumer>
+        <LanguageContext.Consumer>
+          {({ language }) => this.renderSubmit(language)}
+        </LanguageContext.Consumer>
       </button>
     );
   }
