479657e54b338a21e5f445ef827516b6910b1c00
Daniel Vadil <dvadz@yahoo.com>
2022-05-04 05:46:59 -0400

    Updated the usage of LanguageContext inside of Field to destructure "language"

--- src/components/Field.js
+++ src/components/Field.js
@@ -2,15 +2,17 @@ import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Field extends React.Component {
-  renderName(value) {
-    return value === "english" ? "Name" : "Nom";
+  renderName(language) {
+    return language == "english" ? "Name" : "Nom";
   }
 
   render() {
     return (
       <div className='ui field'>
         <label>
-          <LanguageContext.Consumer>{(value) => this.renderName(value)}</LanguageContext.Consumer>
+          <LanguageContext.Consumer>
+            {({ language }) => this.renderName(language)}
+          </LanguageContext.Consumer>
         </label>
         <input />
       </div>
